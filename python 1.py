ad52f24c9ae6e012d9bb4d469da2ef5af7c678c5
madiha2301 <98572727+madiha2301@users.noreply.github.com>
2026-08-25 16:21:16 +0530

    Create python 1.py

---
+++ python 1.py
@@ -0,0 +1,103 @@
+print("Hello, World!")
+
+if 5 > 2:
+  print("Five is greater than two!")
+  
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
+
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+print("I am", 35, "years old.")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+myVariableName = "John" #camel case
+MyVariableName = "John" #pascal case
+my_variable_name = "John" #snake case
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
+
+x = "awesome"
+def myfunc():
+  print("Python is " + x)
+myfunc()
+
+x = "awesome"
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+myfunc()
+print("Python is " + x)
